0b45409057f732ca1be58a12930dcd49ebe44fcb
Sergio Mata <sergiomataherrero@gmail.com>
2023-05-09 18:44:34 +0200

    Add character class and testing

---
+++ app/character.js
@@ -0,0 +1,20 @@
+export class Character {
+  name;
+  houseName;
+  age;
+  status;
+  series;
+
+  constructor(name, houseName, age, status = "alive") {
+    this.name = name;
+    this.houseName = houseName;
+    this.age = age;
+    this.series = "Game of Thrones";
+    this.status = status;
+  }
+
+  die() {
+    this.status = "dead";
+    return this.status;
+  }
+}
